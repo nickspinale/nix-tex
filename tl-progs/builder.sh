34ddbdbdbe45eb3dad7e02d6dc22f177004628a8
Nick Spinale <spinalen@carleton.edu>
2016-01-05 00:24:38 -0600

    raw binaries done

--- tl-progs/builder.sh
+++ tl-progs/builder.sh
@@ -10,3 +10,8 @@ args="--disable-native-texlive-build"
 # args="--disable-all-pkgs --enable-tex --enable-mf --without-mf-x-toolkit --without-x"
 
 TL_INSTALL_DEST=$out TL_CONFIGURE_ARGS=$args ../texlive-20150521-source/Build
+
+mv $out/share/texmf-dist/web2c/texmf.cnf .
+rm -r $out/share/texmf-dist
+mkdir -p $out/share/texmf-dist/web2c
+mv texmf.cnf $out/share/texmf-dist/web2c
