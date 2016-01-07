91d0c89583cf2686c47208e83952bb6574eab181
Nick Spinale <spinalen@carleton.edu>
2016-01-07 15:33:27 -0600

    removed tests

--- default.nix
+++ default.nix
@@ -1,11 +1,7 @@
-{ nixpkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {} }:
 
-{
-  tl-progs = nixpkgs.pkgs.callPackage (import ./tl-progs) {};
+with pkgs;
 
-  tl-src = builtins.fetchTarball {
-    url = ftp://tug.org/texlive/historic/2015/texlive-20150521-source.tar.xz;
-    /* sha256 = "ed9bcd7bdce899c3c27c16a8c5c3017c4f09e1d7fd097038351b72497e9d4669"; */
-    /* md5 = "0sa6kmz4jwhv6lw702gxszhhjkvw071wba0ngk1c76g8vixwv6zd"; */
-  };
+{ tl-progs = callPackage (import ./tl-progs) {};
+  tl-pkgs =  callPackage (import ./tl-pkgs) {};
 }
--- tl-progs/builder.sh
+++ tl-progs/builder.sh
@@ -2,16 +2,10 @@ source $stdenv/setup
 
 tar xf $src
 
-mkdir wat
-cd wat
+mkdir tmp
+cd tmp
 
 args="--disable-native-texlive-build"
-# args="--without-x --disable-native-texlive-build"
-# args="--disable-all-pkgs --enable-tex --enable-mf --without-mf-x-toolkit --without-x"
-
 TL_INSTALL_DEST=$out TL_CONFIGURE_ARGS=$args ../texlive-20150521-source/Build
 
-# mv $out/share/texmf-dist/web2c/texmf.cnf .
 rm -r $out/share/texmf-dist
-# mkdir -p $out/share/texmf-dist/web2c
-# mv texmf.cnf $out/share/texmf-dist/web2c
