57612ee3326d8702c03b31136d6107971d625f1a
Nick Spinale <spinalen@carleton.edu>
2016-01-06 20:21:39 -0600

    cleaned up builders

--- tl-pkgs/tl-pkgs-builder.sh
+++ tl-pkgs/tl-pkgs-builder.sh
@@ -8,22 +8,7 @@ for kind in $default $src $doc; do
     fi
 done
 
-# find "$out" \( \
-#     \( -type f -a -name "*.so*" \) -o \
-#     \( -type f -a -perm -0100 \) \
-#     \) -print -exec patchelf --shrink-rpath '{}' \;
-
 for file in $(find $out * \( -type f -a -executable \)); do
-    current=$(patchelf --print-interpreter $file)
-    # if test $? -eq 0; then
-    patchelf --set-interpreter $glibc$current $file
-    # fi
+    patchelf --set-interpreter $glibc$(patchelf --print-interpreter $file) $file
 done
-
-# for file in $(find $out * -type f); do
-#     current=$(patchelf --print-interpreter $file)
-#     if test $? -eq 0; then
-#         patchelf --set-interpreter $glibc$current $file
-#     fi
-# done
 
